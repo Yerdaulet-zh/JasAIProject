3a497f89769522e0ff22a5c5312ca9628d560058
Yerdaulet <61695069+Yerdaulet-zh@users.noreply.github.com>
2023-04-09 00:25:26 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,34 @@
 import json, torch
-from torchvision import transforms 
-
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 with open("encoder.json", "r") as f:
     encoder = json.load(f)
 
+    
+ocr_transformer = A.Compose([
+    A.Resize(width=112, height=48),
+    # A.Normalize(mean=[133.98177105, 131.71614195, 142.1675694], std=[11.89496205, 12.33475545, 11.7366861]),
+    A.OneOf([
+        A.MotionBlur(p=.2),
+        A.MedianBlur(blur_limit=3, p=0.1),
+        A.Blur(blur_limit=3, p=0.1),
+    ], p=0.2),
+    A.ShiftScaleRotate(shift_limit=0.0525, scale_limit=0.1, rotate_limit=1, p=1),
+    A.GridDistortion(p=.2, border_mode=1),
+    A.OneOf([
+        A.IAASharpen(),
+        A.IAAEmboss(),
+        A.RandomBrightnessContrast(p=0.2),
+    ]),
+    ToTensorV2(),
+])
 
-ocr_transformer = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.52541871, 0.51653389, 0.55751988], std=[0.04664691, 0.04837159, 0.04602622]),
+
+val_transformer = A.Compose([
+    A.Resize(width=112, height=48),
+    # A.Normalize(mean=[133.98177105, 131.71614195, 142.1675694], std=[11.89496205, 12.33475545, 11.7366861]),
+    # ToTensorV2(),
 ])
 
 
